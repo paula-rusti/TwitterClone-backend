080b32ee710c306a40eb72e4724714566af1d269
Paula Rusti <rusti.paula78@gmail.com>
2023-05-03 12:25:33 +0300

    added user model

---
+++ src/main/java/com/example/TwitterClone/models/ApplicationUser.java
@@ -0,0 +1,115 @@
+package com.example.TwitterClone.models;
+
+import jakarta.persistence.*;
+import net.minidev.json.annotate.JsonIgnore;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name="users")
+public class ApplicationUser {
+    // not named User to avoid conflict with Spring security
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_id")
+    private Integer userId;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email", unique = true)
+    private String email;
+
+    @Column(name = "username", unique = true)
+    private String username;
+
+    @JsonIgnore // don't send this back and forth over http
+    private String password_digest;
+
+    // one user can have multiple role and each role cand be possesed by multple users
+    @ManyToMany(fetch = FetchType.EAGER)    // fetch the users role every time we get a user
+    @JoinTable(
+        name="user_role_junction",
+        joinColumns = {@JoinColumn(name = "user_id")},
+        inverseJoinColumns = {@JoinColumn(name = "role_id")}
+    )
+    private Set<Role> authorities;
+
+    public ApplicationUser() {
+        this.authorities = new HashSet<>();
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword_digest() {
+        return password_digest;
+    }
+
+    public void setPassword_digest(String password_digest) {
+        this.password_digest = password_digest;
+    }
+
+    public Set<Role> getAuthorities() {
+        return authorities;
+    }
+
+    public void setAuthorities(Set<Role> authorities) {
+        this.authorities = authorities;
+    }
+
+    @Override
+    public String toString() {
+        return "ApplicationUser{" +
+                "userId=" + userId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", username='" + username + '\'' +
+                ", password_digest='" + password_digest + '\'' +
+                ", authorities=" + authorities +
+                '}';
+    }
+}
