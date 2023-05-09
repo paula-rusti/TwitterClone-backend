f6407b6dfafb3f1fdc50fae24bbc2a12635df8f1
Paula Rusti <rusti.paula78@gmail.com>
2023-05-09 12:33:27 +0300

    modified register user to return a response entity, todo treat the cases when creation fails

--- src/main/java/com/example/TwitterClone/controllers/UserController.java
+++ src/main/java/com/example/TwitterClone/controllers/UserController.java
@@ -5,11 +5,14 @@ import com.example.TwitterClone.models.dto.request.RegistrationRequest;
 import com.example.TwitterClone.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -23,7 +26,7 @@ public class UserController {
 
     // Register a new user
     @PostMapping("/")
-    public ApplicationUser registerUser(@RequestBody RegistrationRequest user) {
+    public ResponseEntity<ApplicationUser> registerUser(@RequestBody RegistrationRequest user) {
         // Check if username and email are unique
         if (userService.existsByUsername(user.getUsername())) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Username already exists");
@@ -32,6 +35,8 @@ public class UserController {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Email already exists");
         }
         // Save the new user
-        return userService.registerUser(new ApplicationUser(user.getFirstName(), user.getLastName(), user.getEmail(), user.getUsername(), user.getPassword())); // tweak constructor
+        ApplicationUser registeredUser = userService.registerUser(new ApplicationUser(user.getFirstName(), user.getLastName(), user.getEmail(), user.getUsername(), user.getPassword())); // tweak constructor
+        return ResponseEntity.created(URI.create("/api/v1/users/" + registeredUser.getUserId())).body(registeredUser);
+        // todo make custom exceptions when username or email are taken
     }
 }
